bf37499e78a19e273d52dcac42dfea250f5acb8b
virtualvolume <anr9999d@gmail.com>
2020-04-18 19:26:43 +0200

    test

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "google" {
+  project = "terraform-274210"
+  region  = "europe-north1"
+  zone    = "europe-north1-b"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = ""
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-10-buster-v20200326"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+    name    = "ssh"
+    network = "default"
+
+    allow {
+        protocol = "tcp"
+        ports    = ["22"]
+    }
+}
+
+
+resource "google_compute_network" "default" {
+  name = "test-network"
+}
