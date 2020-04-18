bcbd29e2e92173dbb041d3d6c57845970578656c
virtualvolume <anr9999d@gmail.com>
2020-04-18 23:05:14 +0200

    Add new VMs

--- main.tf
+++ main.tf
@@ -4,40 +4,50 @@ provider "google" {
   zone    = "europe-north1-b"
 }
 
+// A single Google Cloud Engine instance
 resource "google_compute_instance" "vm_instance" {
-  name         = "hej"
-  machine_type = "f1-micro"
+ name         = "test99"
+ machine_type = "f1-micro"
+ zone         = "europe-north1-a"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-10-buster-v20200326"
-    }
-  }
+ boot_disk {
+   initialize_params {
+     image = "debian-10-buster-v20200326"
+   }
+ }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network       = google_compute_network.vpc_network.self_link
-    access_config {
-    }
-  }
+// Make sure flask is installed on all new instances for later steps
+metadata_startup_script = "sudo apt install -y figlet ; hostname | figlet > /etc/motd"
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+resource "google_compute_instance" "vm_instance100" {
+ name         = "test100"
+ machine_type = "f1-micro"
+ zone         = "europe-north1-a"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-10-buster-v20200326"
+   }
+ }
+
+// Make sure flask is installed on all new instances for later steps
+metadata_startup_script = "sudo apt install -y figlet ; hostname | figlet > /etc/motd"
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
 }
 
-resource "google_compute_firewall" "allow_ssh" {
-    name    = "ssh"
-    network = "default"
-
-    allow {
-        protocol = "tcp"
-        ports    = ["22"]
-    }
-}
-
-
-resource "google_compute_network" "default" {
-  name = "test-network"
-}
