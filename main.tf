881a56b539c5f6506999c31e5beab20736670b59
virtualvolume <anr9999d@gmail.com>
2020-04-19 13:46:23 +0200

    Setup 2 VMs

--- main.tf
+++ main.tf
@@ -4,3 +4,41 @@ provider "google" {
   zone    = "europe-north1-b"
 }
 
+// First VM
+resource "google_compute_instance" "gcp-vm01" {
+ name         = "gcp-vm01"
+ machine_type = "f1-micro"
+ zone         = "europe-north1-a"
+ boot_disk {
+   initialize_params {
+     image = "debian-10-buster-v20200326"
+   }
+ }
+
+// Make sure flask is installed on all new instances for later steps
+metadata_startup_script = "sudo apt install -y figlet ; hostname | figlet > /etc/motd"
+ network_interface {
+   network = "default"
+   }
+}
+
+
+// Second VM
+resource "google_compute_instance" "gcp-vm02" {
+ name         = "gcp-vm02"
+ machine_type = "f1-micro"
+ zone         = "europe-north1-a"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-10-buster-v20200326"
+   }
+ }
+
+// Make sure flask is installed on all new instances for later steps
+metadata_startup_script = "sudo apt install -y figlet ; hostname | figlet > /etc/motd"
+ network_interface {
+   network = "default"
+   }
+}
+
