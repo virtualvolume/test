6e2edb3e3621bb31586a33a0579036af9dbb3564
virtualvolume <anr9999d@gmail.com>
2020-04-20 10:41:12 +0200

    New VMs

--- main.tf
+++ main.tf
@@ -4,3 +4,35 @@ provider "google" {
   zone    = "europe-north1-b"
 }
 
+// First VM
+resource "google_compute_instance" "gcp-vm01" {
+ name         = "gcp-vm01"
+ machine_type = "f1-micro"
+ zone         = "europe-north1-a"
+ boot_disk {
+   initialize_params {
+     image = "debian-10-buster-v20200326"
+   }
+ }
+ network_interface {
+   network = "default"
+   }
+}
+
+
+// Second VM
+resource "google_compute_instance" "gcp-vm02" {
+ name         = "gcp-vm02"
+ machine_type = "f1-micro"
+ zone         = "europe-north1-a"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-10-buster-v20200326"
+   }
+ }
+ network_interface {
+   network = "default"
+   }
+}
+
